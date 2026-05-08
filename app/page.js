ca6b592c9ec4f1eb4c23f77ac80042bb7d7a5d21
Innovate2028 <rafic.mansour@live.com.au>
2026-05-08 14:38:13 +1000

    Create homepage

---
+++ app/page.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <main>
+      <section>
+        <h1>Pilates Her Australia</h1>
+
+        <p>
+          Empowering women across Australia through Pilates,
+          motivation and community.
+        </p>
+
+        <button>Join Community</button>
+      </section>
+    </main>
+  );
+}
